b1feafc7605582b9d0fdb744addbd6be55ca7fca
Ivan Kojić <ikojic000@gmail.com>
2023-08-31 01:51:17 +0200

    first commit

---
+++ pelicanconf.py
@@ -0,0 +1,39 @@
+from datetime import date
+
+
+AUTHOR = "Ivan Kojic"
+SITENAME = "SoftyTech"
+SITEURL = ""
+
+PATH = "content"
+ARTICLE_PATHS = ["blog"]
+ARTICLE_SAVE_AS = "{date:%Y}/{slug}.html"
+ARTICLE_URL = "{date:%Y}/{slug}.html"
+PAGE_PATHS = ["pages"]
+
+TIMEZONE = "Europe/Rome"
+
+DEFAULT_LANG = "English"
+
+# THEME = "minimal-xy"
+THEME = "softyTechTheme"
+
+# Theme customizations
+MINIMALXY_START_YEAR = 2023
+MINIMALXY_CURRENT_YEAR = date.today().year
+
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
+
+# Menu
+MENUITEMS = (("Home", "/"),)
+
+DEFAULT_PAGINATION = 5
+
+# Uncomment following line if you want document-relative URLs when developing
+# RELATIVE_URLS = True
